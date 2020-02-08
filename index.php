6f95921f5858c2fea8696bdbc860af8b2e9891b3
Sycinz <60658488+Sycinz@users.noreply.github.com>
2020-02-08 18:03:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <html>
 
-    <head>
+<head>
 
-        <meta charset="UTF-8"; name="Opis" content="Strona zespołu szkół nr.42 imienia Jana Karskiego"; name="keywords" content="Szkoła, Karski, 42, Zespół, szkół, Technikum, Liceum">
+    <meta charset="UTF-8"; name="Opis" content="Strona zespołu szkół nr.42 imienia Jana Karskiego"; name="keywords" content="Szkoła, Karski, 42, Zespół, szkół, Technikum, Liceum">
+    <title> Zespół Szkół nr.42 imienia Jana Karskiego </title>
+    <link rel="stylesheet" href="KarskiCiemny.css">
 
-        <title> Zespół Szkół nr.42 imienia Jana Karskiego </title>
+</head>
+<body>
 
-        <link rel="stylesheet" href="KarskiCiemny.css">
+<div align = "center"; id="BackgroundUp">
 
-    </head>
+    <h1 aling = "center"> Strona główna | Rodo | Konkursy | Dla nauczycieli | Dla uczniów </h1>
+</div>
+<div align = "center"; id="Photo" >
 
-    <body>
+    <img src="images.jpg" id="PhotoStretch">
+</div>
+<div align = "center"; id = "BackgroundDown">
 
-        <section align = "center"; class="BackgroundUp">
-        
-            <h1 aling = "center"; > Strona główna | Rodo | Konkursy | Dla nauczycieli | Dla uczniów </h1> <a href=""> <button type="button"> Login </button> </a>
+    <h1> i informację </h1>
+</div>
+<div align = "left"; id="Foreground" >
 
-        </section>
+    <a> <h3> Podstrony </h3> </a> <br>
+    <a> <h3> Technik Informatyk </h3> </a> <br>
+    <a> <h3> Technik Turystyki </h3> </a> <br>
+    <a> <h3> Oddział dla młodocianych pracowników </h3> </a>
+</div>
 
-        <section align = "center"; id="Photo" >
-        
-            <img src="./Image/images.jpeg">
-
-        </section>
-
-        <section align = "center"; id="BackgroundDown">
-        
-            <h1> i informację </h1>
-
-        </section>
-
-        <section align = "left"; id="Foreground" >
-        
-            <a> <h3> Podstrony </h3> </a> <br>
-            <a> <h3> Technik Informatyk </h3> </a> <br>
-            <a> <h3> Technik Turystyki </h3> </a> <br>
-            <a> <h3> Oddział dla młodocianych pracowników </h3> </a>
-
-        </section>
-    
-    </body>
-
+</body>
 </html>
